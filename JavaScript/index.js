b48d0d69870eedc1552ecc037781747f044fc2c0
= <=>
2021-05-22 20:45:20 +0530

    initial class

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+// alert("Hello Mahesh")
+console.log("Hello Mahesh")
+
+/* what is js ?
+js is a syncronous and single threaded language,it means execution takes line by line */
+
+console.log("Mithun")
+console.log("Raj")
+console.log("mahesh")
+
+// ctrl+ slash for single comment
+/* multiple comments */
+
