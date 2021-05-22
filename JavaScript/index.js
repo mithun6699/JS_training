959986569d7a983452a35f66d096d71fa206d04c
= <=>
2021-05-22 21:55:57 +0530

    async example

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,6 +6,13 @@ js is a syncronous and single threaded language,it means execution takes line by
 
 console.log("Mithun")
 console.log("Raj")
+setTimeout(()=> {
+    console.log("i am async")
+},5000)
+let b = ()=> {
+    console.log("This is arrow function")
+}
+console.log(b())
 console.log("mahesh")
 
 // ctrl+ slash for single comment
